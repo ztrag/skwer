9a4db87e6fe34484f612c0cffd46a58710ab3049
ztrag <ztrag@protonmail.com>
2023-02-06 09:18:14 -0800

    Color tweak.

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,8 +1,8 @@
 import 'dart:ui';
 
-const Color skRed = Color.fromARGB(255, 255, 0, 122);
-const Color skGreen = Color.fromARGB(255, 105, 210, 0);
-const Color skBlue = Color.fromARGB(255, 0, 122, 255);
+const Color skRed = Color.fromARGB(255, 230, 5, 110);
+const Color skGreen = Color.fromARGB(255, 100, 190, 0);
+const Color skBlue = Color.fromARGB(255, 5, 110, 230);
 const List<Color> skTileColors = [skRed, skGreen, skBlue];
 
 const Color skFocusColor = Color.fromARGB(255, 210, 210, 0);
